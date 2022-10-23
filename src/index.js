9b06ddf69ccc2ba9590461ea5d3567b58b5d2c15
Mahmoud Galal <ssss.aaa2002@gmail.com>
2022-10-23 16:45:05 +0200

    Ready for deployment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const routes= require('../routes/routes')
+const routes= require('../../routes/routes')
 const cors=require('cors')
 
 
